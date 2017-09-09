7e5d6543476352a807e07458bab23735a1d07ef6
Josh Wood <wood2jp2@gmail.com>
2017-09-09 18:47:16 -0400

    curb passport test with handlebars

---
+++ server.js
@@ -0,0 +1,56 @@
+const
+  express = require('express'),
+  app = express(),
+  passport = require('passport'),
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  env = require('dotenv').load(),
+  exphbs = require('express-handlebars');
+
+//For BodyParser
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// For Passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.get('/', function(req, res) {
+  res.send('Welcome to Passport with Sequelize');
+});
+
+// models
+var models = require('./app/models');
+
+// routes
+var authRoute = require('./app/routes/auth.js')(app, passport);
+
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+
+//Sync Database
+models.sequelize.sync().then(function() {
+  console.log('Nice! Database looks fine')
+}).catch(function(err) {
+  console.log(err, "Something went wrong with the Database Update!")
+});
+
+app.listen(5000, function(err) {
+  if (!err)
+    console.log("Site is live");
+  else console.log(err)
+});
