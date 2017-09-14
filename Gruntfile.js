00990e4359600ad422053b238edac22df181816f
Josh Wood <wood2jp2@gmail.com>
2017-09-13 22:11:33 -0400

    travis and grunt packages and files set up

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      options: {
+        outputStyle: 'expanded',
+        sourceMap: true
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'public/sass/',
+          src: '**/*.scss',
+          dest: 'public/css/',
+          ext: '.css'
+        }]
+      }
+    },
+    watch: {
+      files: 'public/sass/**/*.scss',
+      tasks: 'sass'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-travis');
+
+  grunt.registerTask('default', ['sass','watch']);
+
+}
